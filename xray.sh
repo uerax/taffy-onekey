5136e5208ba80f2fad09891f555df1c1829140e3
ww <wei.wang@baishan.com>
2022-11-17 02:31:55 +0800

    修复自动更新失败bug

--- xray.sh
+++ xray.sh
@@ -777,7 +777,7 @@ uninstall() {
 
 update_script() {
   script_path=$(cd `dirname $0`; pwd)
-  wget -N --no-check-certificate -q -O $( readlink -f -- "$0"; ) "https://raw.githubusercontent.com/uerax/xray-script/master/xray.sh"
+  wget --no-check-certificate -q -O $( readlink -f -- "$0"; ) "https://raw.githubusercontent.com/uerax/xray-script/master/xray.sh"
   judge "脚本更新"
 }
 
