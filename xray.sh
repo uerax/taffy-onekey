8cde1db96018a339c14757fe7c31644a201e56b1
crayfish <hentai121@163.com>
2023-03-16 21:17:55 +0800

    be better

--- xray.sh
+++ xray.sh
@@ -19,7 +19,7 @@ Error="${Red}[错误]${Font}"
 
 xray_install_url="https://github.com/uerax/xray-script/raw/master/install-release.sh"
 
-version="1.5"
+version="1.6"
 
 xray_cfg="/usr/local/etc/xray/config.json"
 xray_info="/home/xray/xray_info"
@@ -812,157 +812,11 @@ vless_tcp_xtls_vision() {
 }
 
 vless_tcp_xtls_vision_nginx_cfg() {
-  cat>${nginx_cfg}<<EOF
-http {
-    log_format main '[\$time_local] \$proxy_protocol_addr "\$http_referer" "\$http_user_agent"';
-    access_log /var/log/nginx/access.log main;
-
-    map \$http_upgrade \$connection_upgrade {
-        default upgrade;
-        ""      close;
-    }
-
-    map \$proxy_protocol_addr \$proxy_forwarded_elem {
-        ~^[0-9.]+\$        "for=\$proxy_protocol_addr";
-        ~^[0-9A-Fa-f:.]+\$ "for=\"[\$proxy_protocol_addr]\"";
-        default           "for=unknown";
-    }
-
-    map \$http_forwarded \$proxy_add_forwarded {
-        "~^(,[ \\t]*)*([!#\$%&'*+.^_`\|~0-9A-Za-z-]+=([\!#\$%&'*+.^_`|~0-9A-Za-z-]+|\"([\\t \\x21\\x23-\\x5B\\x5D-\\x7E\\x80-\\xFF]|\\\\[\\t \\x21-\\x7E\\x80-\\xFF])*\"))?(;([!#\$%&'\*+.^_`\|~0-9A-Za-z-]+=([\!#\$%&'*+.^_`|~0-9A-Za-z-]+|\"([\\t \\x21\\x23-\\x5B\\x5D-\\x7E\\x80-\\xFF]|\\\\[\\t \\x21-\\x7E\\x80-\\xFF])*\"))?)*([ \\t]*,([ \\t]*([!#\$%&'\*+.^_`\|~0-9A-Za-z-]+=([\!#\$%&'*+.^_`|~0-9A-Za-z-]+|\"([\\t \\x21\\x23-\\x5B\\x5D-\\x7E\\x80-\\xFF]|\\\\[\\t \\x21-\\x7E\\x80-\\xFF])*\"))?(;([!#\$%&'\*+.^_`\|~0-9A-Za-z-]+=([\!#\$%&'*+.^_`|~0-9A-Za-z-]+|\"([\\t \\x21\\x23-\\x5B\\x5D-\\x7E\\x80-\\xFF]|\\\\[\\t \\x21-\\x7E\\x80-\\xFF])*\"))?)*)?)*\$" "\$http_forwarded, \$proxy_forwarded_elem";
-        default "\$proxy_forwarded_elem";
-    }
-
-    server {
-        listen 80;
-        return 301 https://\$host\$request_uri;
-    }
-
-    server {
-        listen 127.0.0.1:8001 proxy_protocol;
-        listen 127.0.0.1:8002 http2 proxy_protocol;
-        set_real_ip_from 127.0.0.1;
-
-        location / {
-            sub_filter                         \$proxy_host \$host;
-            sub_filter_once                    off;
-
-            proxy_pass                         https://www.lovelive-anime.jp;
-            proxy_set_header Host              \$proxy_host;
-
-            proxy_http_version                 1.1;
-            proxy_cache_bypass                 \$http_upgrade;
-
-            proxy_ssl_server_name on;
-
-            proxy_set_header Upgrade           \$http_upgrade;
-            proxy_set_header Connection        \$connection_upgrade;
-            proxy_set_header X-Real-IP         \$proxy_protocol_addr;
-            proxy_set_header Forwarded         \$proxy_add_forwarded;
-            proxy_set_header X-Forwarded-For   \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-            proxy_set_header X-Forwarded-Host  \$host;
-            proxy_set_header X-Forwarded-Port  \$server_port;
-
-            proxy_connect_timeout              60s;
-            proxy_send_timeout                 60s;
-            proxy_read_timeout                 60s;
-
-            resolver 1.1.1.1;
-        }
-    }
-}
-EOF
-
+  cd /etc/nginx/ && wget -N https://github.com/uerax/xray-script/blob/master/nginx/vless_tcp_xtls_vision.conf -O /etc/nginx/nginc.conf
 }
 
 vless_tcp_xtls_vision_xray_cfg() {
-  cat > ${xray_cfg} << EOF
-{
-    "log": {
-        "loglevel": "warning"
-    },
-    "routing": {
-        "domainStrategy": "IPIfNonMatch",
-        "rules": [
-            {
-                "type": "field",
-                "ip": [
-                    "geoip:cn"
-                ],
-                "outboundTag": "block"
-            }
-        ]
-    },
-    "inbounds": [
-        {
-            "listen": "0.0.0.0", // "0.0.0.0" Indicates listening to both IPv4 and IPv6
-            "port": 443, // The port on which the server listens
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${password}", // User ID, perform xray uuid generation, or a string of 1-30 bytes
-                        "flow": "xtls-rprx-vision"
-                    }
-                ],
-                "decryption": "none",
-                "fallbacks": [
-                    {
-                        "dest": "8001",
-                        "xver": 1
-                    },
-                    {
-                        "alpn": "h2",
-                        "dest": "8002",
-                        "xver": 1
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "tls",
-                "tlsSettings": {
-                    "rejectUnknownSni": true,
-                    "minVersion": "1.2",
-                    "certificates": [
-                        {
-                            "ocspStapling": 3600,
-                            "certificateFile": "${ca_crt}", // For the certificate file, it is recommended to use fullchain (full SSL certificate chain). If there is only a website certificate, v2rayN can be used but v2rayNG cannot be used. Usually, the extension is not distinguished
-                            "keyFile": "${ca_key}" // private key file
-                        }
-                    ]
-                }
-            },
-            "sniffing": {
-                "enabled": true,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ]
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "direct"
-        },
-        {
-            "protocol": "blackhole",
-            "tag": "block"
-        }
-    ],
-    "policy": {
-        "levels": {
-            "0": {
-                "handshake": 2, // The handshake time limit when the connection is established, in seconds, the default value is 4, it is recommended to be different from the default value
-                "connIdle": 120 // Connection idle time limit in seconds, the default value is 300, it is recommended to be different from the default value
-            }
-        }
-    }
-} 
-EOF
+  wget -N https://github.com/uerax/xray-script/blob/master/nginx/vless_tcp_xtls_vision.conf -O xray.conf && mv xray.conf ${xray_cfg}
 }
 
 # XRAY END
@@ -1129,11 +983,11 @@ close_xray() {
 
 server_operation() {
   echo -e "${Green} -------------------------------- ${Font}"
-  echo -e "\t${Green}1)${Font} 重启/启动 Nginx"
-  echo -e "\t${Green}2)${Font} 关闭 Nginx"
-  echo -e "\t${Green}3)${Font} 重启/启动 Xray"
-  echo -e "\t${Green}4)${Font} 关闭 Xray"
-  echo -e "\t${Green}q)${Font} 结束操作\n"
+  echo -e "${Green}1)${Font} 重启/启动 Nginx"
+  echo -e "${Green}2)${Font} 关闭 Nginx"
+  echo -e "${Green}3)${Font} 重启/启动 Xray"
+  echo -e "${Green}4)${Font} 关闭 Xray"
+  echo -e "${Green}q)${Font} 结束操作\n"
   echo -e "${Green} -------------------------------- ${Font}"
   read -rp "输入数字(回车确认): " opt_num
   echo -e ""
@@ -1175,14 +1029,14 @@ question_answer() {
 }
 
 select_type() {
-    echo -e "\t${Green}选择安装的模式 ${Font}"
-    echo -e "\t${Green}1) ${Font} trojan-tcp-tls(推荐)"
-    echo -e "\t${Green}2) ${Font} trojan-grpc"
-    echo -e "\t${Green}3) ${Font} vmess-ws-tls"
-    echo -e "\t${Green}4) ${Font} vless-ws-tls"
-    echo -e "\t${Green}5) ${Font} vless-grpc"
-    echo -e "\t${Green}6) ${Font} vless-tcp-xtls-vision"
-    echo -e "\t${Green}q) ${Font} 不进行操作\n"
+    echo -e "${Green}选择安装的模式 ${Font}"
+    echo -e "${Green}1) ${Font} trojan-tcp-tls(推荐)"
+    echo -e "${Green}2) ${Font} trojan-grpc"
+    echo -e "${Green}3) ${Font} vmess-ws-tls"
+    echo -e "${Green}4) ${Font} vless-ws-tls"
+    echo -e "${Green}5) ${Font} vless-grpc"
+    echo -e "${Green}6) ${Font} vless-tcp-xtls-vision"
+    echo -e "${Green}q) ${Font} 不进行操作\n"
     read -rp "输入数字(回车确认): " menu_num
     echo -e ""
     case $menu_num in
@@ -1220,22 +1074,22 @@ menu() {
     echo -e "\t${Yellow}https://github.com/uerax${Font}"
     echo -e "\t\t${Yellow}版本：${version}${Font}"
     echo -e "——————————————— 安装向导 ———————————————"
-    echo -e "\t${Green}1)${Font} 一键安装"
-    echo -e "\t${Green}2)${Font} 更新脚本"
-    echo -e "\t${Green}3)${Font} 安装/更新 Xray"
-    echo -e "\t${Yellow}4)${Font} 卸载 Xray"
-    echo -e "\t${Green}5)${Font} 安装 Nginx"
-    echo -e "\t${Yellow}6)${Font} 卸载 Nginx"
-    echo -e "\t${Green}7)${Font} 启动 / 关闭 / 重启服务"
-    echo -e "\t${Green}8)${Font} Xray 协议更换"
-    echo -e "\t${Yellow}9)${Font} 完全卸载"
-    echo -e "\t${Green}10)${Font} 配置文件路径"
-    echo -e "\t${Green}11)${Font} 查看配置链接"
-    echo -e "\t${Green}12)${Font} 检测服务状态"
-    echo -e "\t${Green}20)${Font} 更新伪装站"
-    echo -e "\t${Green}99)${Font} 常见问题"
-    echo -e "\t${Green}100)${Font} 开启bbr"
-    echo -e "\t${Green}q)${Font} 退出"
+    echo -e "${Green}1)${Font} 一键安装"
+    echo -e "${Green}2)${Font} 更新脚本"
+    echo -e "${Green}3)${Font} 安装/更新 Xray"
+    echo -e "${Yellow}4)${Font} 卸载 Xray"
+    echo -e "${Green}5)${Font} 安装 Nginx"
+    echo -e "${Yellow}6)${Font} 卸载 Nginx"
+    echo -e "${Green}7)${Font} 启动 / 关闭 / 重启服务"
+    echo -e "${Green}8)${Font} Xray 协议更换"
+    echo -e "${Yellow}9)${Font} 完全卸载"
+    echo -e "${Green}10)${Font} 配置文件路径"
+    echo -e "${Green}11)${Font} 查看配置链接"
+    echo -e "${Green}12)${Font} 检测服务状态"
+    echo -e "${Green}20)${Font} 更新伪装站"
+    echo -e "${Green}99)${Font} 常见问题"
+    echo -e "${Green}100)${Font} 开启bbr"
+    echo -e "${Green}q)${Font} 退出"
     echo -e "————————————————————————————————————————\n"
 
     read -rp "输入数字(回车确认)：" menu_num
