90d5f2c4cb01eb68eb3c974716ed18b55d2559bf
wesker <uerax@live.com>
2025-10-13 03:09:16 +0800

    feat: add clash install

--- install-clash.sh
+++ install-clash.sh
@@ -16,6 +16,26 @@ VERSION=$(curl -s https://api.github.com/repos/MetaCubeX/mihomo/releases/latest 
 
 curl -Lo mihomo.deb "https://github.com/MetaCubeX/mihomo/releases/download/v${VERSION}/mihomo-linux-${ARCH}-v${VERSION}.deb"
 
-sudo dpkg -i mihomo.deb
+dpkg -i mihomo.deb
 rm mihomo.deb
 
+
+cat > /etc/systemd/system/mihomo.service <<EOF
+[Unit]
+Description=mihomo Daemon, Another Clash Kernel.
+After=network.target NetworkManager.service systemd-networkd.service iwd.service
+
+[Service]
+Type=simple
+LimitNPROC=500
+LimitNOFILE=1000000
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
+Restart=always
+ExecStartPre=/usr/bin/sleep 1s
+ExecStart=/usr/bin/mihomo/ -d /etc/mihomo
+ExecReload=/bin/kill -HUP $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
